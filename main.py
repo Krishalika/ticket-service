7204369872dab7f01f4b7352e31e42d147534eba
KrishalikaR <krishalika.r@synergenhealth.com>
2023-12-25 20:43:38 +0530

    initial ticket service

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 
 from router.ticket_router import ticket_router
 
-# stripe.api_key = "sk_test_51OPsooGpa94GP3zWQItoqSmPaZEiPxrtnFwvUuaPS7pdNhiVzeaRGxsoyx9cDpKnR0xRernroxYoOx2jqxNt510k00ykZnARwE"
-
 app = FastAPI()
 
 
